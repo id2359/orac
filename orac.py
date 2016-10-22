4a2720604d3f1181904fa55bd96ff8986d5b1f82
frug <id2359@yahoo.com>
2016-10-22 09:07:48 +0800

    Added GrovelIntent

--- orac.py
+++ orac.py
@@ -8,6 +8,14 @@ ask = Ask(app, "/")
 def launched():
     return question("Orac launched")
 
+@ask.intent("GrovelIntent")
+def grovel():
+    import random
+    grovelling = ["How wonderful to speak with you again, Sir!",
+                  "It is an honour to serve you, sire!"]
+    
+    text = random.choice(grovelling)
+    return statement(text).simple_card("Grovelling", text)
 
 if __name__=="__main__":
     app.run(debug=True)
