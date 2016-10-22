3bed129514c209f615f7b4b1db8b82028a5a974e
frug <id2359@yahoo.com>
2016-10-22 08:29:22 +0800

    Added orac file

---
+++ orac.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from flask_ask import Ask, question, statement
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+
+@ask.launch
+def launched():
+    return question("Orac launched")
